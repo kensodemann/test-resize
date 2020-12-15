f5fad61c788b7999288802b39b4aa4982b636e63
Ken Sodemann <ken@ionic.io>
2020-12-15 12:11:07 -0600

    add android and try options

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,5 @@
 import { Component } from '@angular/core';
-
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Plugins, StatusBarStyle } from '@capacitor/core';
 
 @Component({
   selector: 'app-root',
@@ -10,18 +7,19 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
-  constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
-  ) {
+  constructor() {
     this.initializeApp();
   }
 
   initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
+    const { StatusBar } = Plugins;
+
+    StatusBar.setStyle({
+      style: StatusBarStyle.Dark
+    });
+    StatusBar.setBackgroundColor({ color: '#770000' });
+    StatusBar.setOverlaysWebView({
+       overlay: false
     });
   }
 }
